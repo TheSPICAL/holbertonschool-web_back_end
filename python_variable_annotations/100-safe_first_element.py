0bc4bf63d64818b19fdad35424d8f379046d305c
TheSPICAL <youssefmasakni@gmail.com>
2024-02-12 16:38:12 +0100

    fix

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
-"""
-This is a module that provides a function for returning the first element
-    of a list, or None if the list is empty.
-"""
-from typing import Sequence, Union, Any
+""" Augment the following code with the correct duck-typed annotations:
+    The types of the elements of the input are not know
+    def safe_first_element(lst):
+        if lst:
+            return lst[0]
+        else:
+            return None """
+from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """
-    This function returns the first element of a sequence, or None if the
-        sequence is empty.
-
-    Parameters:
-    lst (Sequence): The sequence to process.
-
-    Returns:
-    Union[object, None]: The first element of the sequence, or None if the
-        sequence is empty.
-    """
+    """ Duck typing - first element of a sequence """
     if lst:
         return lst[0]
     else:
