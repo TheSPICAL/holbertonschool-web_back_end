e81288cb0bae4f67ae5cf56cbb38c7b871e2d7cf
TheSPICAL <youssefmasakni@gmail.com>
2024-02-12 16:33:17 +0100

    fix

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-""" Augment """
+"""
+Duck typing - first element of a sequence
+"""
 from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """ Duck typing - first element of a sequence """
+    """
+    safe first element
+    """
     if lst:
         return lst[0]
     else:
