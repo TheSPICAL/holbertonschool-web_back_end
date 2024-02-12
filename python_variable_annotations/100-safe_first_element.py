ddde008cab73183b26097640720cd595aa615ace
TheSPICAL <youssefmasakni@gmail.com>
2024-02-12 16:36:10 +0100

    fix

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 """
-Duck typing - first element of a sequence
+This is a module that provides a function for returning the first element
+    of a list, or None if the list is empty.
 """
-from typing import Sequence, Any, Union
+from typing import Sequence, Union, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
-    safe first element
+    This function returns the first element of a sequence, or None if the
+        sequence is empty.
+
+    Parameters:
+    lst (Sequence): The sequence to process.
+
+    Returns:
+    Union[object, None]: The first element of the sequence, or None if the
+        sequence is empty.
     """
     if lst:
         return lst[0]
